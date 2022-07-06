e8edafb6c1dd4aa767ae309153c946413c72ee95
Nitin K <nitink61996@gmail.com>
2022-07-06 10:47:22 +0530

    read the large data file with node createReadStream to parse

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const { parser } = require('csv-parse');
+const fs = require('fs');
 
-console.log(parser);
+const results = [];
+
+// reading the file using the stream
+// this will read the file and gives back array of buffers - these are objects
+// which node uses to represent bits and bytes of information
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(`Something went wrong ${err}`);
+    })
+    .on('end', () => {
+        console.log('Results : ', results);
+
+        console.log('End of file stream');
+    });
