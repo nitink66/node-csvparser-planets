e80cdb4914677b26129a91dd8b43fe58d2349e79
Nitin K <nitink61996@gmail.com>
2022-07-06 11:45:16 +0530

    Exploring habitable planets - validation

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const fs = require('fs');
 
 const results = [];
 
+const isHabitable = (planet) => {
+    //koi_insol - stellar flux is amount of light that planet gets for survival of life
+    //koi_prad - how large a planet can be
+    return (
+        planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 &&
+        planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6
+    );
+};
+
 // reading the file using the stream
 // this will read the file and gives back array of buffers - these are objects
 // which node uses to represent bits and bytes of information
@@ -14,13 +25,18 @@ fs.createReadStream('kepler_data.csv')
         })
     )
     .on('data', (data) => {
-        results.push(data);
+        if (isHabitable(data)) {
+            results.push(data);
+        }
     })
     .on('error', (err) => {
         console.log(`Something went wrong ${err}`);
     })
     .on('end', () => {
-        console.log('Results : ', results);
-
+        console.log(
+            'Habitable planets found : ',
+            results.map((planetItem) => planetItem['kepler_name'])
+        );
+        console.log('Habitable planets found : ', results.length);
         console.log('End of file stream');
     });
